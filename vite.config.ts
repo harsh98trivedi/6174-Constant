ae0c826f9e3135476b597edf094046bfc4ca4b6d
Harsh Trivedi <harsh98trivedi@gmail.com>
2026-03-12 07:35:16 +0530

    Fixed Base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/6174-Constant/',
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
